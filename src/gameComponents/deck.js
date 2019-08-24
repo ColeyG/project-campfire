daf0047018d215b9f869a71a6e709a8587fbdd14
ColeyG <cjgeerts@gmail.com>
2019-08-24 19:11:39 -0400

    Add deck drawing

--- src/gameComponents/deck.js
+++ src/gameComponents/deck.js
@@ -3,10 +3,34 @@ import "../../styles/deck.scss";
 const image = require("../../resources/Plebes/V2_4x/PixelPlebes_V2_4x__53.png");
 
 class Deck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardsInDeck: 5,
+      fauxCards: [],
+    };
+  }
+  componentDidMount() {
+    let fauxCards = [];
+    for (let i = 0; i < this.state.cardsInDeck; i++) {
+      fauxCards.push(
+        <div key={i} className="deck-card">
+          <img src={"compiled/" + image} alt="Deck Back Art" />
+        </div>
+      );
+    }
+    this.setState({ fauxCards: fauxCards });
+  }
+  drawCard = () => {
+    let newDeck = this.state.fauxCards;
+    newDeck.shift();
+    this.setState({ fauxCards: newDeck });
+    console.log(this.state.fauxCards);
+  };
   render() {
     return (
-      <div className="deck">
-        <img src={"compiled/" + image} alt="Deck Back Art" />
+      <div className="deck" onClick={this.drawCard}>
+        {this.state.fauxCards}
       </div>
     );
   }
